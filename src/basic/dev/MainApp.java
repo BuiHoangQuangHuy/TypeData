258b064e15d5a4985a09f6a95ed56bac0921d4c7
BuiHoangQuangHuy <82139984+BuiHoangQuangHuy@users.noreply.github.com>
2021-05-06 14:46:11 +0700

    Update MainApp.java

--- src/basic/dev/MainApp.java
+++ src/basic/dev/MainApp.java
@@ -1,5 +1,31 @@
 package basic.dev;
 
 public class MainApp {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+		String nameStudent;
+		String idStudent;
+		String cmndStudent;
+		String nameClassStu;
+		String nameSchoolStu;
+		float diemTrungBinh;
+		
+		
+		
+		String namePerson;
+		String adressPerson;
+		String numberPhonePerson;
+		float heSoLuongPerson;
+		
+		
+		String nameCar;
+		String hangCar;
+		int giaCar;
+		String colorCar;
+
+	}
 
 }
